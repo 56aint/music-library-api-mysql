381ac8fc4f844d1011c7ae8fc9403c7ef25285e5
aar.56aint <aaron.hays_7@yahoo.com>
2020-05-27 20:36:57 +0100

    edited include to exclude artist when searching for songs by albumId

--- src/controllers/songs.js
+++ src/controllers/songs.js
@@ -27,16 +27,16 @@ exports.getSongsByAlbumId = (req, res) => {
     if (!song) {
       res.status(404).json({ error: 'The album could not be found.' });
     } else {
-      /*Song.findAll({ where: { albumId } })
-        .then(songs => { res.status(200).json(songs); });*/
+      /* Song.findAll({ where: { albumId } })
+        .then(songs => { res.status(200).json(songs); }); */
 
 
-      Song.findAll({ include: [{ model: Artist, as: 'artist' }, { model: Album, as: 'album' }] }).then((songs) => {
+      /* Song.findAll({ include: [{ model: Artist, as: 'artist' }, { model: Album, as: 'album' }] }).then((songs) => {
         res.status(200).json(songs);
-      });
+      }); */
       // console.log(songs);
 
-
+      Song.findAll({ where: { albumId }, include: [{ model: Artist, as: 'artist' }, { model: 'Album', as: 'album' }] });
     }
   });
 };
