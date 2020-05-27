b6c6de8d9f34ff46e53d1f77b530dd749b2cac94
aar.56aint <aaron.hays_7@yahoo.com>
2020-05-27 22:42:35 +0100

    configured heroku with include keyword

--- src/controllers/songs.js
+++ src/controllers/songs.js
@@ -1,7 +1,7 @@
 const { Song } = require('../sequelize');
 const { Artist } = require('../sequelize');
 const { Album } = require('../sequelize');
-// const models;
+
 exports.createSongByAlbumId = (req, res) => {
   const { albumId } = req.params;
 
@@ -22,19 +22,10 @@ exports.createSongByAlbumId = (req, res) => {
 };
 exports.getSongsByAlbumId = (req, res) => {
   const { albumId } = req.params;
-  // console.log(albumId);
   Album.findByPk(albumId).then(song => {
     if (!song) {
       res.status(404).json({ error: 'The album could not be found.' });
     } else {
-      /* Song.findAll({ where: { albumId } })
-        .then(songs => { res.status(200).json(songs); }); */
-
-      /* Song.findAll({ include: [{ model: Artist, as: 'artist' }, { model: Album, as: 'album' }] }).then((songs) => {
-        res.status(200).json(songs);
-      }); */
-      // console.log(songs);
-
       Song.findAll({ where: { albumId }, include: [{ model: Artist, as: 'artist' }, { model: Album, as: 'album' }] }).then((songs) => {
         res.status(200).json(songs);
       });
@@ -44,14 +35,12 @@ exports.getSongsByAlbumId = (req, res) => {
 
 exports.getSongsByArtistId = (req, res) => {
   const { artistId } = req.params;
-  // console.log(albumId);
   Artist.findByPk(artistId).then(artist => {
     if (!artist) {
       res.status(404).json({ error: 'The artist could not be found.' });
     } else {
       Song.findAll({ include: [{ model: Artist, as: 'artist' }, { model: Album, as: 'album' }] }).then((songs) => {
         res.status(200).json(songs);
-        // console.log(songs);
       });
     }
   });
